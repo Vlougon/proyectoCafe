aa76cd3dbf388297b6adfd8c1d7cae1e89f2f776
victor <vlougongs@gmail.com>
2023-11-17 19:15:01 +0000

    Actividad 21. Ejercicio 2: Hecho!

---
+++ database/migrations/2023_11_17_185259_create_modulos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modulos', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText('codigo', 3);
+            $table->tinyText('materia');
+            $table->unsignedTinyInteger('h_semanales');
+            $table->unsignedTinyInteger('h_totales');
+            $table->foreignId('user_id');
+            $table->foreignId('especialidad_id');
+            $table->foreignId('curso_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modulos');
+    }
+};
