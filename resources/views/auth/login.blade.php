b70e0d76bb06519afc5977b9cdd66a9d7d07b4da
victor <vlougongs@gmail.com>
2023-12-14 17:36:38 +0000

    Minor Style Changes!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
                             <path fill="#454545" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
                         </svg>
                     </span>
-                    <input type="email" name="email" id="email" placeholder="Usuario, NIF/NIE o CIAL" autocomplete="on">
+                    <input type="email" name="email" id="email" placeholder="Usuario, NIF/NIE o CIAL" value="{{old('email')}}" autocomplete="on">
                 </div>
 
                 <div id="passwordBox">
@@ -44,7 +44,7 @@
                             <path fill="#454545" d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" />
                         </svg>
                     </span>
-                    <input type="password" name="password" id="password" placeholder="Contraseña" autocomplete="off">
+                    <input type="password" name="password" id="password" placeholder="Contraseña" value="{{old('password')}}" autocomplete="off">
                 </div>
 
                 <button type="submit" id="submitBox" name="Botón para Iniciar Sesión">
